959e66a9e7e89cbe786eefd9d47852d1052d21f5
tachun lin <creed88@gmail.com>
2014-06-18 23:22:51 +0200

    Seperate page, add splash screens

--- www/js/_config.js
+++ www/js/_config.js
@@ -6,43 +6,48 @@
 //////////////////////////
 
 if (window.location.host == 'facebookmobileweb.com' || window.location.host == 'www.facebookmobileweb.com') {
-  var gAppID = '147366981996453';
+    var gAppID = '147366981996453';
 }
 //Add your Application ID here
 else {
-  var gAppID = '269377779842002';
+    var gAppID = '269377779842002';
 }
 
-if (gAppID == 'enter_your_appid_here') {
-  alert('You need to enter your App ID in js/_config.js on line 13.');
+if (gAppID == '1496568253892641') {
+    alert('You need to enter your App ID in js/_config.js on line 13.');
 }
 
 //Initialize the Facebook SDK
 //See https://developers.facebook.com/docs/reference/javascript/
 window.fbAsyncInit = function() {
-  FB.init({ 
-    appId: gAppID,
-    status: true,
-    cookie: true,
-    xfbml: true,
-    frictionlessRequests: true,
-    useCachedDialogs: true,
-    oauth: true
-  });
+    FB.init({
+        appId: gAppID,
+        status: true,
+        cookie: true,
+        xfbml: true,
+        frictionlessRequests: true,
+        useCachedDialogs: true,
+        oauth: true
+    });
 
-  FB.getLoginStatus(handleStatusChange);
+    FB.getLoginStatus(handleStatusChange);
 
-  authUser();
-  checkForCredits();
-  updateAuthElements();
+    authUser();
+    checkForCredits();
+    updateAuthElements();
 };
 
 
 // Load the SDK Asynchronously
-(function(d){
- var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
- if (d.getElementById(id)) {return;}
- js = d.createElement('script'); js.id = id; js.async = true;
- js.src = "//connect.facebook.net/en_US/all.js";
- ref.parentNode.insertBefore(js, ref);
+(function(d) {
+    var js, id = 'facebook-jssdk',
+        ref = d.getElementsByTagName('script')[0];
+    if (d.getElementById(id)) {
+        return;
+    }
+    js = d.createElement('script');
+    js.id = id;
+    js.async = true;
+    js.src = "//connect.facebook.net/zh_TW/all.js";
+    ref.parentNode.insertBefore(js, ref);
 }(document));
